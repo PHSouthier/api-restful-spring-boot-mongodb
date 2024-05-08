15e946834980b309513b2396476283784edfba1b
Pedro Southier <phsouthier@hotmail.com>
2024-05-07 21:52:42 -0300

    project created

---
+++ src/test/java/com/phs/apirestfulmongodb/ApirestfulmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phs.apirestfulmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
